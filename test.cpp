b2e653524aa8dbd99f949c3c1c9b57d74cd8e686
Ranjit Pandey <ranjit.pandey@novascotia.ca>
2026-03-04 20:26:09 +0000

    poc complete stuffs working

---
+++ test.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+#include "transit_realtime.pb.h"
+#include<fstream>
+#include<curl/curl.h>
+
+using namespace std;
+using namespace transit_realtime;
+
+size_t write_data(void* ptr, size_t size, size_t nmemb, void* stream) {
+    std::ofstream* out = static_cast<std::ofstream*>(stream);
+    out->write(static_cast<char*>(ptr), size * nmemb);
+    return size * nmemb;
+}
+
+string downloadFile(const string url_path = "https://gtfs.halifax.ca/realtime/Vehicle/VehiclePositions.pb", const string fileName = "VehiclePositions.pb"){
+
+	CURL* curl;
+	CURLcode res;
+
+	curl_global_init(CURL_GLOBAL_DEFAULT);
+	curl = curl_easy_init();
+
+	if(curl){
+		fstream file(fileName, fstream::out | fstream::binary);
+		if(!file.is_open()) {
+			cerr << "Error creating the file." << endl;
+		}
+		
+		curl_easy_setopt(curl, CURLOPT_URL, url_path.c_str());
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &file);
+
+		res = curl_easy_perform(curl);
+
+		if(res != CURLE_OK){
+			cerr << "Err downloading the file" << curl_easy_strerror(res) << endl;
+		}
+
+		file.close();
+		curl_easy_cleanup(curl);
+	}
+
+	curl_global_cleanup();
+
+	return fileName;
+
+}
+
+string getValueFromTag(char* args[], string tag){
+
+ 	for(int i = 0; args[i] != nullptr; i++){
+		cout << args[i]<< " : "<< (bool)(args[i] == "-f")<<endl;
+		if(strcmp(args[i], "-f")){
+			if(args[i+1] != nullptr) return args[i+1];
+			cerr<<"file not provided after the -f flag"<<endl;
+		}
+	}
+	return ""; 
+}
+
+bool isFileProvidedAlredy(char* args[]){
+	return !getValueFromTag(args, "-f").empty();
+}
+
+int main(int argc, char* args[]){
+	// download the file if its not passed already
+	string fileName = isFileProvidedAlredy(args) ? getValueFromTag(args, "-f") : downloadFile();
+
+	ifstream file(fileName, ios::binary);
+
+	if(!file.is_open()){
+		cerr<< "error reading the file"<< endl;
+	}
+
+	FeedMessage feed;
+	if (!feed.ParseFromIstream(&file)) {
+        	std::cerr << "Failed to parse feed message." << std::endl;
+        	return 1;
+    }
+
+	for (int i = 0; i < feed.entity_size(); i++) {
+        	FeedEntity entity = feed.entity(i);
+        
+        	if (entity.has_vehicle()) {
+            		std::cout << "Vehicle: "<< entity.vehicle().trip().route_id() <<" Position: " 
+                      	<< entity.vehicle().position().latitude() << ", "
+                      	<< entity.vehicle().position().longitude() << ", " 
+		      	<< entity.vehicle().position().odometer()<< ", "
+		      	<< entity.vehicle().position().speed()<< ", "
+			<< std::endl;
+        	}
+    }
+}
