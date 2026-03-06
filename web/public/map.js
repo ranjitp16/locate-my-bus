c9f988d8f203023ad9f186f3360cbfb662b8b1de
Ranjit Parshuram Pandey <ranjit@mac.home>
2026-03-06 18:42:03 -0400

    polls the data every 30 seconds and redraws the map

--- web/public/map.js
+++ web/public/map.js
@@ -14,14 +14,27 @@ const customIcon = L.divIcon({
     iconAnchor: [2, 2],
 })
 
-fetch('/1')
-    .then(res => res.json())
-    .then(vehicles => {
-        let avgLat = vehicles.reduce((sum, v) => sum + v.lat, 0) / vehicles.length
-        let avgLon = vehicles.reduce((sum, v) => sum + v.lon, 0) / vehicles.length
-        map.setView([avgLat, avgLon], 13)
-        vehicles.forEach(v => {
-            console.log("added:", v)
-            L.marker([v.lat, v.lon], { icon: customIcon }).addTo(map).bindPopup(`Route ID: ${v.route_id}<br>Vehicle ID: ${v.vehicle_id}<br>Timestamp: ${v.timestamp}<br>Speed: ${v.speed}<br>Bearing: ${v.bearing}`)
+let markers = []
+fetxhFromAPI();
+
+setInterval(() => {
+    markers.forEach(m => m.remove())
+    markers = []
+    fetxhFromAPI()
+}, 30000); // Update every 30 seconds
+
+function fetxhFromAPI() {
+    fetch('/10A')
+        .then(res => res.json())
+        .then(vehicles => {
+            let avgLat = vehicles.reduce((sum, v) => sum + v.lat, 0) / vehicles.length
+            let avgLon = vehicles.reduce((sum, v) => sum + v.lon, 0) / vehicles.length
+            map.setView([avgLat, avgLon], 13)
+            vehicles.forEach(v => {
+                const ageSeconds = (Date.now() - Date.parse(v.timestamp)) / 1000
+                const marker = L.marker([v.lat, v.lon], { icon: customIcon })
+                    .addTo(map).bindPopup(`Route: ${v.route_id}<br>Vehicle ID: ${v.vehicle_id}<br>Timestamp: ${ageSeconds} s ago<br>Speed: ${v.speed} m/s<br>Bearing: ${v.bearing}`)
+                markers.push(marker)
+            })
         })
-    })
+}
